1866acabfe8af38c87b54b5d2b708a054e7486c1
aldair9475 <patricioaldair690@gmail.com>
2025-03-12 19:19:17 -0500

    agregando services y servicesimpl

---
+++ src/main/java/services/MedicamentoService.java
@@ -0,0 +1,10 @@
+package services;
+
+import java.util.List;
+import models.Medicamento;
+
+public interface MedicamentoService {
+	
+	List<Medicamento> listarMedicamentos();
+
+}
---
+++ src/main/java/services/impl/MedicamentoServiceImpl.java
@@ -0,0 +1,16 @@
+package services.impl;
+
+import java.util.List;
+
+import models.Medicamento;
+import services.MedicamentoService;
+
+public class MedicamentoServiceImpl implements MedicamentoService{
+
+	@Override
+	public List<Medicamento> listarMedicamentos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
