123f57580d1d3cd675cf7b8d069047dbec8e6cc4
curibe777 <curibe@isise.edu.pe>
2025-03-13 22:36:00 -0500

    actualizando servlet

--- src/main/java/controllers/MedicamentosServlet.java
+++ src/main/java/controllers/MedicamentosServlet.java
@@ -11,10 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Marca;
 import models.Productos;
+import models.Categoria;
+import models.Presentacion;
 import services.MarcaService;
 import services.ProductoService;
 import services.impl.MarcaServiceImpl;
 import services.impl.ProductoServiceImpl;
+import services.CategoriaService;
+import services.PresentacionService;
+import services.impl.CategoriaServiceImpl;
+import services.impl.PresentacionServiceImpl;
 
 @WebServlet("/medicamentos")
 public class MedicamentosServlet extends HttpServlet {
@@ -22,22 +28,32 @@ public class MedicamentosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private final ProductoService productoService;
 	private final MarcaService marcaService;
+	private final CategoriaService categoriaService;
+	private final PresentacionService presentacionService;
        
     
     public MedicamentosServlet() {
         super();
         productoService = new ProductoServiceImpl();
         marcaService = new MarcaServiceImpl();
+        categoriaService = new CategoriaServiceImpl();
+        presentacionService = new PresentacionServiceImpl();
     }
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		List<Productos> productos = productoService.listarProductos();
-		request.setAttribute("medicamentos", productos);
+		request.setAttribute("productos", productos);
 		
 		List<Marca> marcas = marcaService.listarMarcas();
 		request.setAttribute("marcas", marcas);
+		
+		List<Categoria> categorias = categoriaService.listarCategorias();
+		request.setAttribute("categorias", categorias);
+		
+		List<Presentacion> presentacion = presentacionService.listarPresentacion();
+		request.setAttribute("presentacion", presentacion);
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
 		dispatcher.forward(request, response);
