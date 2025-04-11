3a0edc2fed653cb06718bb735f81268892dda8c9
aldair9475 <patricioaldair690@gmail.com>
2025-04-11 00:39:42 -0500

    agregando carrito servlet

--- src/main/java/controllers/CarritoServlet.java
+++ src/main/java/controllers/CarritoServlet.java
@@ -1,38 +1,94 @@
 package controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Categoria;
+import models.Producto;
+import models.Venta;
+import models.VentaDetalle;
+import services.impl.CategoriaServiceImpl;
+import services.impl.ProductoServiceImpl;
+import services.impl.VentaDetalleServiceImpl;
+import services.impl.VentaServiceImpl;
 import shared.Util;
 
 
 @WebServlet("/carrito")
 public class CarritoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private VentaServiceImpl ventaServiceImpl;
+	private VentaDetalleServiceImpl ventaDetalleServiceImpl;
+	private ProductoServiceImpl productoServiceImpl;
+	private CategoriaServiceImpl categoriaServiceImpl;
        
    
     public CarritoServlet() {
         super();
-       
+        ventaServiceImpl = new VentaServiceImpl();
+        ventaDetalleServiceImpl = new VentaDetalleServiceImpl();
+    	productoServiceImpl = new ProductoServiceImpl();
+    	categoriaServiceImpl = new CategoriaServiceImpl();
     }
 
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String sessionId = Util.getCookieValue(request.getCookies(), "JSESSIONID");
+		Venta venta = ventaServiceImpl.obtenerVentaPorCodigoInterno(sessionId);
+		
+		List<VentaDetalle> ventasDetalle = ventaDetalleServiceImpl.listarVentaDetalleCarrito(venta.getIdVenta());
+		List<Categoria> categorias = categoriaServiceImpl.listarCategorias();
+		
+		request.setAttribute("categorias", categorias);
+		request.setAttribute("ventasDetalle", ventasDetalle);
+		
+		 RequestDispatcher dispatcher = request.getRequestDispatcher("/views/carrito.jsp");
+		 dispatcher.forward(request, response);
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Integer idProducto = Integer.parseInt(request.getParameter("idProducto"));
-		System.out.println("Se esta agregando un elemento al carrito: " + idProducto);
+		System.out.println("Se esta agregando un elemento al carrito idProducto : " + idProducto);
 
 		String sessionId = Util.getCookieValue(request.getCookies(), "JSESSIONID");
-		System.out.println("getCookieValue : "+sessionId);
+		System.out.println("sessionId : "+sessionId);
+		
+		Venta venta = ventaServiceImpl.obtenerVentaPorCodigoInterno(sessionId);
+		if(venta==null) {
+			Venta newVenta = new Venta();
+			newVenta.setCodigoInterno(sessionId);
+			venta = ventaServiceImpl.insertarVenta(newVenta);
+		}
+		
+		System.out.println("Ventaid::"+venta.getIdVenta());
+		
+		VentaDetalle ventaDetalle = ventaDetalleServiceImpl.obtenerVentaDetallePorIdVentaYIdProducto(venta.getIdVenta(), idProducto); 
+		Producto producto = productoServiceImpl.obtenerProductoPorIdProducto(idProducto);
+		if(ventaDetalle == null) {
+			VentaDetalle newVentaDetalle = new VentaDetalle();
+			newVentaDetalle.setIdVenta(venta.getIdVenta());
+			newVentaDetalle.setIdProducto(idProducto);
+			newVentaDetalle.setPrecioUnitario(producto.getPrecioUnitario());
+			newVentaDetalle.setSubtotal(producto.getPrecioUnitario());
+			ventaDetalle = ventaDetalleServiceImpl.insertarVentaDetalle(newVentaDetalle);
+		} else {
+			ventaDetalleServiceImpl.aumentarVentaDetalleCantidad(ventaDetalle.getIdVentaDetalle());
+			ventaDetalleServiceImpl.actualizarVentaDetalleSubTotal(ventaDetalle.getIdVentaDetalle());
+		}
+		
+		ventaServiceImpl.actualizarVentaMontoTotal(venta.getIdVenta());
+		System.out.println("VentaDetalleId::"+ventaDetalle.getIdVentaDetalle());
+		
+		response.getWriter().append("OK");
 
 	}
 
---
+++ src/main/java/models/VentaDetalle.java
@@ -0,0 +1,108 @@
+package models;
+
+public class VentaDetalle {
+
+	private int idVentaDetalle;
+	private int idVenta;
+	private int idProducto;
+	private int cantidad;
+	private double precioUnitario;
+	private double subtotal;
+	private double igv;
+	private String nombreProducto;
+	private String imagenProducto;
+	private String nombrePresentacion;
+	private double montoTotal;
+
+	public int getIdVentaDetalle() {
+		return idVentaDetalle;
+	}
+
+	public void setIdVentaDetalle(int idVentaDetalle) {
+		this.idVentaDetalle = idVentaDetalle;
+	}
+
+	public int getIdVenta() {
+		return idVenta;
+	}
+
+	public void setIdVenta(int idVenta) {
+		this.idVenta = idVenta;
+	}
+
+	public int getIdProducto() {
+		return idProducto;
+	}
+
+	public void setIdProducto(int idProducto) {
+		this.idProducto = idProducto;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public double getIgv() {
+		return igv;
+	}
+
+	public void setIgv(double igv) {
+		this.igv = igv;
+	}
+
+	public String getNombreProducto() {
+		return nombreProducto;
+	}
+
+	public void setNombreProducto(String nombreProducto) {
+		this.nombreProducto = nombreProducto;
+	}
+
+	public String getImagenProducto() {
+		return imagenProducto;
+	}
+
+	public void setImagenProducto(String imagenProducto) {
+		this.imagenProducto = imagenProducto;
+	}
+
+	public String getNombrePresentacion() {
+		return nombrePresentacion;
+	}
+
+	public void setNombrePresentacion(String nombrePresentacion) {
+		this.nombrePresentacion = nombrePresentacion;
+	}
+
+	public double getMontoTotal() {
+		return montoTotal;
+	}
+
+	public void setMontoTotal(double montoTotal) {
+		this.montoTotal = montoTotal;
+	}
+
+	
+}
